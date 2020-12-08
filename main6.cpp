b147931541839f4ec0ec5f9138a510c70bcca446
JohnFreakKennedy <62018542+JohnFreakKennedy@users.noreply.github.com>
2020-12-08 02:51:38 +0200

    Update main6.cpp

--- main6.cpp
+++ main6.cpp
@@ -34,5 +34,70 @@ int main() {
     W(a,b);
     return 0;
     }
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 
+using namespace std;
+
+
+
+
+int main() {
+    cout<<"Dankov Artem,IS-01,27-V";
+    double a,b;
+    double U(double a,double b);
+    double V(double a,double b);
+    double W(double a, double b);
+    cout<<"Put first digit a: ";
+    cin>>a;
+    cout<<"Put second digit b: ";
+    cin>>b;
+    W(a,b);
+    return 0;
+}
+double U(double a,double b)
+{
+    double min=0;
+    if(a<b) {
+        min=a;
+        return min;
+    }
+    else {
+        min=b;
+        return min;
+        cout<<"U="<<b<<endl;
+    }
+}
+
+double V(double a,double b)
+{
+    double min=0;
+    if(a*b<a+b) {
+        min=a*b;
+        return a*b;
+        cout<<"V="<<a*b<<endl;
+    }
+    else {
+        min=a+b;
+        return a+b;
+        cout<<"V="<<a+b<<endl;
+    }
+}
+
+void W(double a,double b)
+{
+    cout<<"U="<<U(a,b)<<endl;
+    cout<<"V="<<V(a,b)<<endl;
+    double min;
+    double c = U(a,b)+V(a,b)*V(a,b);
+    if (c<M_PI) {
+        min=c;
+        cout<<"W="<<c;
+    }
+    else {
+        min=M_PI;
+        cout<<"W="<<M_PI;
+    }
+}
